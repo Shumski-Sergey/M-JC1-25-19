655e09b52e0d7d6c1f788f486a45c6852b123aec
Ivan <yavanya92@gmail.com>
2019-12-19 00:03:46 +0300

    added comments

--- src/iyakushevich/Lesson3_A2.java
+++ src/iyakushevich/Lesson3_A2.java
@@ -6,13 +6,15 @@ import static java.lang.Math.*;
 
 public class Lesson3_A2 {
     public static void main(String[] args) {
+
+        //read parameters from terminal
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextInt();
         double b = scanner.nextInt();
         double c = scanner.nextInt();
 
-        double x1, x2;
-        double d = b * b - 4 * a * c;
+        double x1, x2;  //this is our variables
+        double d = b * b - 4 * a * c; //discriminant
 
         if (d > 0) {
             x1 = (((-b) + sqrt(d)) / (2 * a));
