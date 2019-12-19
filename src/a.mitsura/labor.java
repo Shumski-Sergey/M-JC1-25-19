34d1d53c980bad0aaf2156fe4c00d9c14086d049
gerveis <rouje135@mail.ru>
2019-12-19 03:43:42 +0300

    3:40 AM. My second cup of coffee has disappeared like the first one. It's gotta be magic

---
+++ src/a.mitsura/labor.java
@@ -0,0 +1,14 @@
+package a.mitsura;
+
+public class labor {
+    public static void main(String[] args){
+        int a=15;
+        int b=14;
+        int c=10;
+        if (Math.abs(a-c)<Math.abs(b-c)) {
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
