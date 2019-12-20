5084df701acb2a9f9a5470576de31d710fd974a3
InnaRomankova <Romankova03071978@gmail.com>
2019-12-20 17:42:21 +0300

    new files

---
+++ src/iromankova/lab2/Task_3_B2.java
@@ -0,0 +1,9 @@
+package iromankova.lab2;
+
+public class Task_3_B2 {
+    public static void main(String[] args) {
+        for (int i=5; i>0; i--){
+            System.out.print(i);
+        }
+    }
+}
