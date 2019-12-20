4258a35f4c8c3bed686632f5d7505ad2791f0cf0
Tatsiana Murashka <murashkotatsiana@gmail.com>
2019-12-20 16:47:29 +0300

    Lab 3 tasks

---
+++ src/tmurashka/lab3/lab3_B2.java
@@ -0,0 +1,9 @@
+package tmurashka.lab3;
+
+public class lab3_B2 {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
