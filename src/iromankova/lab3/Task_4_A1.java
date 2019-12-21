b05887214d6621d6db6940c11f374758f05b80dd
InnaRomankova <Romankova03071978@gmail.com>
2019-12-21 22:47:18 +0300

    New Task_4_A1.java added

---
+++ src/iromankova/lab3/Task_4_A1.java
@@ -0,0 +1,20 @@
+package iromankova.lab3;
+
+//Имеется целое число. Следует посчитать сумму цифр данного числа.
+import java.util.Scanner;
+
+public class Task_4_A1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число:");
+        int number = scanner.nextInt();
+        int sum = 0; // переменная для подсчета суммы цифр числа
+        int a; // переменная для записи остатка от деления по модулю
+        while (number>0) {
+            a = number % 10;
+            sum=sum+a;
+            number/=10;
+        }
+        System.out.println("Сумма цифр данного числа: "+sum);
+    }
+}
