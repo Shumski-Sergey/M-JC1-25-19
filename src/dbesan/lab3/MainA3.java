ed70ec682b2e0712f639416d63867294689a7548
jarlaksy <jarlaksy@gmail.com>
2019-12-21 15:34:32 +0300

    A3 не дорешан

---
+++ src/dbesan/lab3/MainA3.java
@@ -0,0 +1,17 @@
+package dbesan.lab3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by user on 21.12.2019.
+ */
+public class MainA3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите число");
+        int a = Integer.parseInt(reader.readLine());
+
+    }
+}
