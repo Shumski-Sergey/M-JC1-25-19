29a1e346bbec120fb08458bb0b5af892357e6110
Alexlifeby <1985alexlife>
2019-12-21 15:21:02 +0300

    First task of third lection

--- src/irusy/Task3Zad1.java
+++ src/irusy/Task3Zad1.java
@@ -2,12 +2,13 @@ package src.irusy;
 
 
 public class Task3Zad1 {
-    public static void main (String[] args){
-        int k = 12345; int x = k;
+    public static void main(String[] args) {
+        int k = 53289;
+        int x = k;
         int s = 0;
-        while (x != 0 ){
-            s +=x%10;
-            x /=10;
+        while (x != 0) {
+            s += x % 10;
+            x /= 10;
         }
         System.out.println("Сумма цифр в числе " + k + " = " + s);
     }
