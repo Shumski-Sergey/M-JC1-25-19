60ecaf1a61ee63426c8e3027aa3c427af4ca26fb
InnaRomankova <Romankova03071978@gmail.com>
2019-12-22 11:27:03 +0300

    Task_4_A2.java added

---
+++ src/iromankova/lab3/Task_4_A2.java
@@ -0,0 +1,28 @@
+package iromankova.lab3;
+
+import java.util.Scanner;
+
+/* Имеется целое число, определить является ли это число простым,
+т.е. делится без остатка только на 1 и себя.*/
+public class Task_4_A2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число:");
+        int number = scanner.nextInt();
+        if (number <= 1) {
+            System.out.println("Число " + number + " не является простым числом");
+        } else {
+            int count = 0; // переменная для подсчета делений без остатка
+            for (int i = 2; i <= number; i++) {
+                if (number % i == 0) {
+                    count += 1;
+                }
+            }
+            if (count == 1) {
+                System.out.println("Число " + number + " является простым числом");
+            } else {
+                System.out.println("Число " + number + " не является простым числом");
+            }
+        }
+    }
+}
