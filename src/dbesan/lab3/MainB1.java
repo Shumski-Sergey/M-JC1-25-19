b64929b34f5bc9b4d6b5fed435af08d07c69eb8b
mxfrei <dmitry19@inbox.ru>
2019-12-23 03:00:23 +0300

    Решение всех задач lab 3

--- src/dbesan/lab3/MainB1.java
+++ src/dbesan/lab3/MainB1.java
@@ -5,15 +5,13 @@ import java.io.*;
 public class MainB1 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите число");
-        int a = Integer.parseInt(reader.readLine());
-        int x = 1;
-        for (int i = 1; i<=a; i++)
-        { x = x*i;
+        System.out.println("Считаем факториал. Введите число");
+        int a = Integer.parseInt(reader.readLine()); //читаем строку и сразу парсим в число
+        int x = 1; //результат
+        for (int i = 1; i <= a; i++) {
+            x = x * i; //вычисляем факториал
         }
-        System.out.print(x);
-
-
+        System.out.print("Фрактал числа" + a + "составляет" + x);
 
 
     }
