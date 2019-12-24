21a689d80d4a7be37a4d7aa7788044a79cb2e3af
InnaRomankova <Romankova03071978@gmail.com>
2019-12-24 09:46:48 +0300

    New Task_4_A3.java added

---
+++ src/iromankova/lab3/Task_4_A3.java
@@ -0,0 +1,27 @@
+package iromankova.lab3;
+
+/*3. Имеется целое число, следует вывести его в бухгалтерском формате.
+Т.е. начиная справа каждые три позиции отделяются пробелом. Например число
+20023143 должно быть выведено как 20 023 143 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_4_A3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите целое число:");
+        String string = reader.readLine();
+        System.out.println("Бухгалтерский формат введенного числа:");
+        /* С введенным числом будем работать, как со строкой (string).
+        С помощью метода charAt будем выводить на печать символы поочередно,
+        начиная слева. */
+        for (int i = 0, j = string.length() - 1; i < string.length(); i++, j--) {
+            System.out.print(string.charAt(i));
+            if (j % 3 == 0 & j > 0) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
