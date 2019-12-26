34cf2830936c59e4ecbf0cacd86e6964684acc62
katekropa11 <katekropa@gmail.com>
2019-12-26 14:34:53 +0300

    Task B2 in lesson 3 and task b1 in lesson 4

--- src/KKropa/Lab_1/Lesson_3_B2.java
+++ src/KKropa/Lab_1/Lesson_3_B2.java
@@ -1,12 +1,12 @@
 package KKropa.Lab_1;
 
-import java.util.Scanner;
+
 
 public class Lesson_3_B2 {
     public static void main (String [] args) {
         int i=5;
         while (i>=1){
-            System.out.println(i-- + " ");
+            System.out.print(i-- + " ");
         }
     }
 }
---
+++ src/KKropa/Lab_3/Lesson_4_B1.java
@@ -0,0 +1,21 @@
+package KKropa.Lab_3;
+
+
+import java.util.Scanner;
+
+public class Lesson_4_B1 {
+    public static void main (String [] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число:");
+        int num=in.nextInt();
+
+        int newNum=1;
+        int i=1;
+
+        while (i<=num){
+            newNum*=i;
+            i++;
+        }
+        System.out.print(newNum);
+    }
+}
