e3305e68a8246f2d8812e187ff04d6d9d0be9424
Dmitry Grigorchik <demidrollo@gmail.com>
2019-12-27 22:21:49 +0300

    3 labs are finished

---
+++ src/d/grigirchik/lab2/Task1.java
@@ -0,0 +1,9 @@
+package d.grigirchik.lab2;
+
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
---
+++ src/d/grigirchik/lab2/Task2.java
@@ -0,0 +1,11 @@
+package d.grigirchik.lab2;
+
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        for (int i=1; i<=5;i++){
+            System.out.println("Я начинаю изучвть Java!");
+        }
+    }
+}
