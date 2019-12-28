d83f103c50cdc9cdc95c620bebe11fb688f328a1
gerveis <59026635+gerveis@users.noreply.github.com>
2019-12-28 20:13:02 +0300

    begin tasks

---
+++ src/a.mitsura/closest_number.java
@@ -0,0 +1,13 @@
+package a.mitsura;
+
+public class closest_number {
+    public static void main(String[] args){
+        double m=8.5;
+        double n=11.45;
+        if (Math.abs(m-10)>Math.abs(n-10)){
+            System.out.println(n+" ближе к 10, чем "+m);
+        }else{
+            System.out.println(m+" ближе к 10, чем "+n);
+        }
+    }
+}
