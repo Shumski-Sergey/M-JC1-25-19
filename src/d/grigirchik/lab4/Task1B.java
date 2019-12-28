47e7b65e79adc98c0cac800f02fb63e487924970
Dmitry Grigorchik <demidrollo@gmail.com>
2019-12-28 03:31:56 +0300

    add new Tasks (lab4)

--- src/d/grigirchik/lab4/Task1B.java
+++ src/d/grigirchik/lab4/Task1B.java
@@ -2,28 +2,21 @@ package d.grigirchik.lab4;
 
 import java.util.Scanner;
 
-public class Task2A {
+public class Task1B {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number;
-        int counter=0;
+        int factorial=1;
         System.out.println("Hello user! Please enter a number");
         while (!scanner.hasNextInt()) {
             System.out.println("Sorry, you enter not a number. Please try again");
             scanner.next();
         }
         number = scanner.nextInt();
-        System.out.println("You enter: " + number);
-        for (int i =1; i <= number; i++) {
-            if (number % (i) == 0) {
-                counter=counter+1;
-            }
+        System.out.println("You enter a number: "+ number);
+        for (int i=1; i<=number; i++){
+            factorial=factorial*i;
         }
-        if (counter>2){
-            System.out.println("This number: "+number+" isn't prime");
-        }
-        else {
-            System.out.println("This number: "+number+" is prime");
-        }
+        System.out.println("Your namber factorial: "+ factorial);
     }
 }
--- src/d/grigirchik/lab4/Task2B.java
+++ src/d/grigirchik/lab4/Task2B.java
@@ -2,28 +2,21 @@ package d.grigirchik.lab4;
 
 import java.util.Scanner;
 
-public class Task2A {
+public class Task2B {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number;
-        int counter=0;
         System.out.println("Hello user! Please enter a number");
         while (!scanner.hasNextInt()) {
             System.out.println("Sorry, you enter not a number. Please try again");
             scanner.next();
         }
         number = scanner.nextInt();
-        System.out.println("You enter: " + number);
-        for (int i =1; i <= number; i++) {
-            if (number % (i) == 0) {
-                counter=counter+1;
+        System.out.println("Your number can be divided without a remainder into these numbers:");
+        for (int i=1; i<=number; i++){
+            if (number%i==0){
+                System.out.println("number " +i);
             }
-        }
-        if (counter>2){
-            System.out.println("This number: "+number+" isn't prime");
-        }
-        else {
-            System.out.println("This number: "+number+" is prime");
         }
     }
 }
