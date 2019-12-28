ec8581ccb482b8d3441f47408be1950c0c257f4f
gerveis <59026635+gerveis@users.noreply.github.com>
2019-12-28 20:21:31 +0300

    Add files via upload

---
+++ Massiv2.java
@@ -0,0 +1,9 @@
+package a.mitsura;
+
+public class Massiv2 {
+    public static void main(String [] args){
+        for(int n=0, i=1; n<55; i+=2, n++){
+            System.out.println(i+", ");
+        }
+    }
+}
