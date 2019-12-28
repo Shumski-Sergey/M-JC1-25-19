da7d4c7be1b7a20a23b99f17cbab0e5439997375
user <SergeyKarpen>
2019-12-28 14:35:39 +0300

    M-JC1-25-19-updates lab1-lab3

---
+++ src/skarpen/Lab1.java
@@ -0,0 +1,12 @@
+package src.skarpen;
+
+/**
+ * Created by user on 28.12.2019.
+ */
+public class Lab1 {
+    public static void main(String[] args) {
+/*        for (int i = 0; i < 5; i++) {
+            System.out.println("Helloy world");
+        }*/
+    }
+}
