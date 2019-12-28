1b6e8576867c8869c659b70a622925d7e8bca099
gerveis <59026635+gerveis@users.noreply.github.com>
2019-12-28 20:12:11 +0300

    Massive tasks

---
+++ src/a.mitsura/Massiv4.java
@@ -0,0 +1,9 @@
+package a.mitsura;
+
+public class Massiv4 {
+    public static void main(String [] args){
+        for(int i=2, n=0; n<20; n++, i*=2){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/a.mitsura/Massiv5.java
@@ -0,0 +1,9 @@
+package a.mitsura;
+// хз как получилось
+public class Massiv5 {
+    public static void main(String [] args){
+        for(int a=2; a<10000; a=2*a-1){
+            System.out.println(a);
+        }
+    }
+}
