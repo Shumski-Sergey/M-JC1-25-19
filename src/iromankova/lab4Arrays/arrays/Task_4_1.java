8739a0edd4e2582a5b37a60485da0049f7567af8
Inna Romankova <Romankova03071978@gmail.com>
2019-12-29 16:25:01 +0300

    Task_4_1.java are corrected

--- src/iromankova/lab4Arrays/arrays/Task_4_1.java
+++ src/iromankova/lab4Arrays/arrays/Task_4_1.java
@@ -1,28 +1,37 @@
 package iromankova.lab4Arrays.arrays;
+// 1.В задаче на поиск максимальной оценки, вывести не саму оценку, а ее номер.
 
 import java.util.Random;
 
 public class Task_4_1 {
     public static void main(String[] args) {
         int[] arrayMarks = new int[7]; // Создаем массив для внесения значений оценок, например, на 7 элементов
-        System.out.print("У нас имеются оценки: ");
-        Random random = new Random();
-        for (int i = 0; i < arrayMarks.length; i++) {
+        System.out.print("У нас есть оценки: ");
+        Random random = new Random(); // Оценки будем получать рандомно
+        for (int i = 0; i < arrayMarks.length; i++) { // В этом цикле получим оценки рандомно
             arrayMarks[i] = random.nextInt(10);
         }
-        for (int x : arrayMarks) {
+        for (int x : arrayMarks) { // Цикл выводит на печать значения полученных рандомно оценок
             System.out.print(x + " ");
         }
         System.out.println();
-        System.out.print("Максимальная оценка под номером: ");
-        int maxMark = arrayMarks[0];
-        int numberOfMark = 0;
-        for (int i = 1; i < arrayMarks.length; i++) {
+        System.out.print("Максимальная оценка под номером (номерами): ");
+        int maxMark = arrayMarks[0]; // Создаем переменную, которой впоследствии присвоим значение максимальной оценки
+        for (int i = 1; i < arrayMarks.length; i++) { // В этом цикле определим значение максимальной оценки
             if (arrayMarks[i] > maxMark) {
                 maxMark = arrayMarks[i];
-                numberOfMark = i;
             }
         }
-        System.out.println(numberOfMark);
+        int[] arrayMaxNumbers = new int[7]; // Создаем новый массив для номеров максимальных оценок
+        for (int i = 0; i < arrayMarks.length; i++) { // Максимальных оценок может быть несколько, поместим их номера в массив
+            if (arrayMarks[i] == maxMark) {
+                arrayMaxNumbers[i] = i + 1; // В этот массив помещаем только номера максимальных оценок, остальные элементы этого массива будут равны "0"
+            }
+        }
+        for (int i = 0; i < arrayMarks.length; i++) { // Цикл для вывода на печать номера (номеров) максимальных оценок
+            if (arrayMaxNumbers[i] > 0) {
+                System.out.print(arrayMaxNumbers[i] + " ");
+            }
+        }
     }
 }
