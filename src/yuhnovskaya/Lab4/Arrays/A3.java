94b53e4dc7c45ce80a1d58c1fe182735886e0fd7
Yuhnovskaya <Yuhnovskaya@gmail.com>
2020-01-03 00:14:53 +0300

    A3 from Arrays Lab4 fixed

--- src/yuhnovskaya/Lab4/Arrays/A3.java
+++ src/yuhnovskaya/Lab4/Arrays/A3.java
@@ -5,7 +5,7 @@ package yuhnovskaya.Lab4.Arrays;
 public class A3 {
     public static void main(String[] args) {
         //array declaration and filling
-        int[] array = new int[9];
+        int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 10);
             System.out.print(array[i] + " ");
