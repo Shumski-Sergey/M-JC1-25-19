873bcfd6bcc87fae80b37097f1b64caefd9218ab
skarpen <1488karpen@gmail.com>
2020-01-03 20:27:09 +0300

    M-JC1-25-19-add file Lab5_8

---
+++ src/skarpen/lesson_5/Lab5_8.java
@@ -0,0 +1,33 @@
+package src.skarpen.lesson_5;
+
+public class Lab5_8 {
+    public static void main(String[] args) {
+
+        //Создать массив, заполнить его случайными элементами, распечатать,
+        //перевернуть, и снова распечатать (при переворачивании нежелательно создавать
+        //еще один массив)
+
+        /*
+        System.out.println("Create array random:");
+        int[] array = new int[14];
+        for (int i = 0; i < array.length; i++){
+            array[i] = (int) (Math.random() * 11); //[0; 10] - Array value range
+            System.out.print(array[i] + " ");
+        }
+
+        int l = array.length;
+        //Переменная, равная длине массива 14 элементов;
+
+        for (int x = 0; x < array.length/2; x++) {
+            int array2 = array[l-x-1];
+            array[l-x-1] = array[x];
+            array[x] = array2;
+        }
+        //Выводим конечный массив
+        System.out.println( "\n" +  "Reserve array:");
+        for (int i=0; i < array.length; i++) {
+            System.out.print( array[i] + " ");
+        }
+        */
+    }
+}
