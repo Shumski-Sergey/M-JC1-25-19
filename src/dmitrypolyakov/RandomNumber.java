0a91a7505e0522b68b04bce1caa01db551f8febf
DmityPol/Dim <github.com>
2020-01-04 19:17:19 +0300

    M-JC1-25-19 Some lecture tasks completed
    Tasks for lectures 2, 3

---
+++ src/dmitrypolyakov/RandomNumber.java
@@ -0,0 +1,13 @@
+package dmitrypolyakov;
+/**
+ * Java courses
+ *
+ * @author Dmitry Polyakov (M-JC1-25-19)
+ * @version dated 18.12.2019
+ */
+public class RandomNumber {
+    public static void main(String[] args) {
+        int a = (int) (Math.random() * 21 );
+        System.out.println("Случайное число: " + a);
+    }
+}
