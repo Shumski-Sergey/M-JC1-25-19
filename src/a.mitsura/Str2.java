614f8546270fa3a967ba0d703c16ef73ea1a3358
gerveis <59026635+gerveis@users.noreply.github.com>
2020-01-09 00:52:19 +0300

    Strings Lesson. Task #2

--- src/a.mitsura/Str2.java
+++ src/a.mitsura/Str2.java
@@ -1,4 +1,9 @@
 package a.mitsura;
 
 public class Str2 {
+    public static void main(String [] args){
+        String s="I don't know a shit what to write for the test";
+    String[] s2= s.split(" +");
+        System.out.println(s2.length);
+    }
 }
