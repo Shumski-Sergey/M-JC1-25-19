267fbe7970e8a456867d53e54501ba23a5cb242b
skarpen <1488karpen@gmail.com>
2020-01-08 21:24:58 +0300

    M-JC1-25-19-app file Lab6_3

---
+++ src/skarpen/lesson_6/Lab6_3.java
@@ -0,0 +1,41 @@
+package src.skarpen.lesson_6;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Lab6_3 {
+    public static void main(String[] args) {
+        //Ввести n строк с консоли. Вывести на консоль те строки, длина которых меньше (больше) средней, а также длину.
+     /*   ArrayList<String> far = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        boolean pip = true;
+        do {
+            System.out.println("Input string:");
+            far.add(sc.nextLine());
+            System.out.println("continue: Yes/No ?");
+            if (!sc.nextLine().equalsIgnoreCase("Yes")) {
+                pip = false;
+            }
+        }
+        while (pip);
+        {
+            String max = far.get(0);
+            String min = far.get(0);
+            for (int i = 0; i < far.size(); i++) {
+                if (min.length() > far.get(i).length()) {
+                    min = far.get(i);
+                } else if (max.length() < far.get(i).length()) {
+                    max = far.get(i);
+                }
+            }
+            System.out.println("The longest string is: \n" + max + " and " + "length " + max.length());
+            System.out.println("The shortest string is: \n" + min + " and " + "length " + min.length());
+        }
+        System.out.println("The end.");
+*/
+        }
+
+
+    }
+
