b4f8c2a8e8eb481e73bd09016d2eb483904455cd
SergeyKarpen <1488karpen@gmail.com>
2020-01-10 16:07:45 +0300

    M-JC1-25-19-create file Lab6_2_3

---
+++ src/skarpen/lesson_6/Lab6_2_3.java
@@ -0,0 +1,27 @@
+package src.skarpen.lesson_6;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Lab6_2_3 {
+    public static void main(String[] args) {
+        //Имеется строка с текстом. Вывести текст, составленный из последних букв всех слов
+       /*
+        System.out.println("Input text:");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String string = "([а-я[А-Я]]+)|([a-z[A-Z]]+)";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Pattern pattern = Pattern.compile(string);
+        Matcher matcher = pattern.matcher(str);
+
+        while (matcher.find()) {
+            stringBuilder.append(str.substring(matcher.end()-1, matcher.end()));
+        }
+        System.out.println(stringBuilder);
+        */
+    }
+}
+
