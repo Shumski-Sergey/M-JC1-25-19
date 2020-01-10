367f198f9751aa5b5dcda2c09b4d37dc9c18124f
gerveis <59026635+gerveis@users.noreply.github.com>
2020-01-11 00:52:55 +0300

    Strings Lesson. Task #2

---
+++ src/a.mitsura/Str1.java
@@ -0,0 +1,13 @@
+package a.mitsura;
+
+import java.util.Scanner;
+public class Str1 {
+    public static void main(String[] args) {
+        Scanner enter = new Scanner(System.in);
+        System.out.println("Enter your text");
+        String text=enter.nextLine();
+        int a=text.length();
+        int b=text.replaceAll("[,.?!-;]", "").length();
+        System.out.println(a-b);
+    }
+}
--- src/a.mitsura/Str3.java
+++ src/a.mitsura/Str3.java
@@ -3,10 +3,10 @@ package a.mitsura;
 public class Str3 {
     public static void main(String[] args) {
         String s1 = "I always knew my purpose";
-        String[] s2 = s1.split(" +");
-        StringBuilder word = new StringBuilder();
+        String[] s2 = s1.split(" +"); //Разбивает строку на ячейки массива
+        StringBuilder word = new StringBuilder(); //Создание StrBuilder'a
         for(String dev:s2){
-            word.append(dev.charAt(dev.length()-1));
+            word.append(dev.charAt(dev.length()-1));//CharAt возвращает символ(последнюю букву) из массива строки по указанному индексу
         }
         System.out.print(word);
     }
