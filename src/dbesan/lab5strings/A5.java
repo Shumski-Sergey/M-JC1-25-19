7f69c5375846554ac5a635ac27635e007e637481
mxfrei <dmitry19@inbox.ru>
2020-01-10 14:55:19 +0300

    Several solutions

--- src/dbesan/lab5strings/A5.java
+++ src/dbesan/lab5strings/A5.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 
 public class A5 {
-    public static void main(String[] args) {}} /*
+    public static void main(String[] args) {
 
         Pattern pattern = Pattern.compile("[ ]"); //считаем слова по пробелу
         Pattern patternend = Pattern.compile("\\z");//считаем последнее слово
@@ -23,7 +23,7 @@ public class A5 {
         int latcount = 0;//счетчик слов с латиницей
                 for (int i = 0; i < arrStr.length; i++) //цикл считает сколько слов с латиницей, чтобы сделать массив
                 {
-            Pattern pattern1 = Pattern.compile("^[a-zA-Z]$");
+            Pattern pattern1 = Pattern.compile("[a-zA-Z]$");
             Matcher matcher1 = pattern1.matcher(arrStr[i]);
                     while (matcher1.find())
                     {latcount++;}
@@ -31,7 +31,7 @@ public class A5 {
         String[] arrLat = new String[latcount];// сюда пишутся латинские слова
         for (int i = 0; i < arrStr.length; i++) //цикл ищет латинские слова и пишет их в массив
         {
-             Pattern pattern2 = Pattern.compile("^[a-zA-Z]$");
+             Pattern pattern2 = Pattern.compile("[a-zA-Z]$");
             Matcher matcher2 = pattern2.matcher(arrStr[i]);
             while (matcher2.find())
             {for(int j = 0; j<arrLat.length; j++) //непосредственно запись
@@ -48,28 +48,24 @@ public class A5 {
             {
                 length = arrStr[i].length();
             }}
-        String[] arrtmp = new String[length];
+
         int finalcount = 0;//вывести количесто слов с равным количеством гласных и согласных - это счетчик
+
         for (int i = 0; i < arrLat.length; i++) {
            for (char character : arrLat[i].toCharArray()) {
-             arrtmp[i] = String.valueOf(character);//возможно не верно, тест не представляется возможным, ибо проблема выше
-            }
+
             Pattern pattern3 = Pattern.compile("[aeiouyAEIOUY]");
-            Matcher matcher3 = pattern3.matcher(arrtmp[i]);
+            Matcher matcher3 = pattern3.matcher(String.valueOf(character));
             Pattern pattern4 = Pattern.compile("[qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNM]");
-            Matcher matcher4 = pattern4.matcher(arrtmp[i]);
+            Matcher matcher4 = pattern4.matcher(String.valueOf(character));
             while (matcher3.find()) {
                 glcount++;
             }
             while (matcher4.find()) {
                 soglcount++;
-            }
+            }}
             if(glcount==soglcount) {finalcount++;}
         }
         System.out.println(finalcount);
     }
         }
-*/
-
-
-
