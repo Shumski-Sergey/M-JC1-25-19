dd4c8002527934c23edfbac22c319bb4d07dd602
Yuhnovskaya <Yuhnovskaya@gmail.com>
2020-01-10 16:08:42 +0300

    A3 from Strings added

--- src/yuhnovskaya/Lab5/Strings/A3.java
+++ src/yuhnovskaya/Lab5/Strings/A3.java
@@ -1,4 +1,21 @@
 package yuhnovskaya.Lab5.Strings;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//3. Имеется строка с текстом. Вывести текст, составленный из последних букв
+//всех слов
 public class A3 {
+    public static void main(String[] avgs){
+        System.out.print("Enter a text: ");
+        Scanner scanner=new Scanner(System.in);
+        String text=scanner.nextLine();
+        Pattern pattern=Pattern.compile("[a-zA-Z]\\b");
+        Matcher last_symb=pattern.matcher(text);
+        while (last_symb.find()) {
+
+            System.out.print(text.substring(last_symb.start(), last_symb.end()));
+        }
+    }
 }
