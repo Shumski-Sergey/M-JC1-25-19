58e3224e9d4e68c49e4dcbf8f16fe9154d1c844c
SergeyKarpen <1488karpen@gmail.com>
2020-01-10 19:02:16 +0300

    M-JC1-25-19-file Lab6_2_2

--- src/skarpen/lesson_6/Lab2_2.java
+++ src/skarpen/lesson_6/Lab2_2.java
@@ -1,17 +1,15 @@
 package src.skarpen.lesson_6;
 
-import javafx.scene.transform.Scale;
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lab2_2 {
     public static void main(String[] args) {
-/*
+
         //   2. Имеется строка с текстом. Подсчитать количество слов в тексте. Желательно учесть, что слова могут разделяться несколькими пробелами, в начале и конце
         //  текста также могут быть пробелы, но могут и отсутствовать
+/*
         System.out.println("Input string");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
@@ -24,4 +22,5 @@ public class Lab2_2 {
         }
         System.out.println(d);
 */
-    }}
+    }
+}
