ab3b680f9250be65510f24ccc961708d7afb6ce3
sshumsky <shumski.s.a@gmail.com>
2020-01-11 14:19:30 +0300

    Create car class

---
+++ src/sshumski/Car.java
@@ -0,0 +1,22 @@
+package sshumski;
+
+public class Car {
+    private String brand;
+    private String name;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
