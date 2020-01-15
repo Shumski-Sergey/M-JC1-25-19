a184288a8b6c0ef35ce43d56afa055a3a9165923
sshumsky <shumski.s.a@gmail.com>
2020-01-15 21:35:47 +0300

    Add mini example for OOP

---
+++ src/sshumski/dogs/Dog.java
@@ -0,0 +1,47 @@
+package sshumski.dogs;
+
+public class Dog {
+    private String name;
+    private int age;
+    private int weight;
+    private int power;
+
+    public Dog(String name, int age, int weight, int power) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+}
