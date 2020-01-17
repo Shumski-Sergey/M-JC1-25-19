ca84b2ac16e910b450960885685dfd43a0e9726d
DmityPol/Dim <github.com>
2020-01-17 19:41:12 +0300

    M-JC1-25-19 Some lecture tasks completed
    Tasks for lectures Arrays

---
+++ src/dmitrypolyakov/TaskArrays/Loops1.java
@@ -0,0 +1,19 @@
+package dmitrypolyakov.TaskArrays;
+
+/**
+ * Java courses
+ *
+ * @author Dmitry Polyakov (M-JC1-25-19)
+ * @version dated 17.01.2020
+ *
+ * Создайте программу, выводящую на экран все четырёхзначные числа последовательности 1000 1003 1006 1009 1012 1015 …
+ */
+
+
+public class Loops1 {
+    public static void main(String[] args) {
+        for (int a = 1000; a <= 2000; a = a + 3) {
+            System.out.print(a + " ");
+        }
+    }
+}
