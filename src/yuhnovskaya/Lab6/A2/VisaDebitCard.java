9295222f2bba54162859671f6e58256ce6effe7c
Yuhnovskaya <Yuhnovskaya@gmail.com>
2020-01-18 00:26:52 +0300

    A2 from Lab6 added

--- src/yuhnovskaya/Lab6/A2/VisaDebitCard.java
+++ src/yuhnovskaya/Lab6/A2/VisaDebitCard.java
@@ -4,7 +4,7 @@ public class VisaDebitCard extends DebitCard{
     public VisaDebitCard(String bank, String owner, double byrTOusd, double usdTOrub, int cashback){
         super(bank,owner,byrTOusd,usdTOrub,cashback);
     }
-    public double conve_byrTOrub() {
+    public double conv_byrTOrub() {
         double rub=byrTOusd*usdTOrub;
         return rub;}
 }
