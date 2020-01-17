2e6e399eca17f49f70b85f9542f2845aa387ec54
SergeyKarpen <1488karpen@gmail.com>
2020-01-17 15:25:55 +0300

    M-JC1-25-19-create hierarchy

---
+++ src/skarpen/hierarchy/Test.java
@@ -0,0 +1,36 @@
+package src.skarpen.hierarchy;
+
+import java.util.ArrayList;
+
+public class Test  {
+    public static void main(String[] args) {
+
+        ArrayList <TechniquesSmall> techniquesSmall = new ArrayList<>();
+
+        TechniquesSmall techniquesSmallFirst = new TechniquesSmall("Kettle", 2018, 1.5);
+        TechniquesSmall techniquesSmallSecond = new TechniquesSmall("Microwave", 2020, 2.1);
+        TechniquesSmall techniquesSmallThird = new TechniquesSmall("Toaster", 2018, 0.5);
+
+        techniquesSmall.add(techniquesSmallFirst);
+        techniquesSmall.add(techniquesSmallSecond);
+        techniquesSmall.add(techniquesSmallThird);
+
+        for (TechniquesSmall product:techniquesSmall) {
+            System.out.println(product.Info());
+            System.out.println(product.countDate());
+            System.out.println(product.needDelivery());
+        }
+        
+        ArrayList <TechniquesBig> techniquesBig = new ArrayList<>();
+        TechniquesBig techniquesBigFirst = new TechniquesBig("Refrigerator", 2019, 50.5);
+        TechniquesBig techniquesBigSecond = new TechniquesBig("Washer", 2015, 25.0);
+        TechniquesBig techniquesBigThird = new TechniquesBig("Stove", 2010, 32.2);
+
+        techniquesBig.add(techniquesBigFirst);
+        techniquesBig.add(techniquesBigSecond);
+        techniquesBig.add(techniquesBigThird);
+
+        System.out.println(techniquesBig.get(0).Info() +"\n" + techniquesBig.get(0).countDate()+ "\n" + techniquesBig.get(0).needDelivery());
+
+    }
+}
