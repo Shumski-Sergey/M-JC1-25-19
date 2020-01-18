5e84a1cdf6b6f10474d6528d92a34ca56a1329f2
Pavel <pavanaindra@mail.ru>
2020-01-18 23:16:07 +0200

    Выполнена НЕПРАВИЛЬНО!

--- src/pvetashkov/lab6/time/service/impl/TimeServiceImpl.java
+++ src/pvetashkov/lab6/time/service/impl/TimeServiceImpl.java
@@ -2,7 +2,6 @@ package pvetashkov.lab6.time.service.impl;
 import pvetashkov.lab6.time.Time;
 import pvetashkov.lab6.time.service.TimeService;
 public class TimeServiceImpl implements TimeService{
-   //private Object longiness;
 
     public TimeServiceImpl() {
     }
@@ -11,11 +10,7 @@ public class TimeServiceImpl implements TimeService{
     public int vivodtotal(Time time, Time timetwo) {
         return dlinatotal(time)>dlinatotal(timetwo)? dlinatotal(time):dlinatotal(timetwo);
     }
-    //private int dlinatotaltwo(Time time) {return time.getHours()*3600+time.getMinutes()*60+time.getSeconds();}
-   // @Override
-   // public int vivodtotaltwo(Time timetwo){
-   //     return dlinatotal(timetwo);
-   // }
+
     private String dlina (Time time) {return time.getNomer();}
     @Override
     public String vivod(Time time, Time timetwo) {
