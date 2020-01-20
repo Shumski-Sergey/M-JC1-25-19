2a9dacd2d280bd95c30d801f9ad8668992cdda8f
SergeyKarpen <1488karpen@gmail.com>
2020-01-20 18:00:34 +0300

    M-JC1-25-19-Test

--- src/skarpen/hierarchy/Test.java
+++ src/skarpen/hierarchy/Test.java
@@ -1,27 +1,12 @@
 package src.skarpen.hierarchy;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Test  {
+public class Test {
     public static void main(String[] args) {
-
-        ArrayList <TechniquesSmall> techniquesSmall = new ArrayList<>();
-
-        TechniquesSmall techniquesSmallFirst = new TechniquesSmall("Kettle", 2018, 1.5);
-        TechniquesSmall techniquesSmallSecond = new TechniquesSmall("Microwave", 2020, 2.1);
-        TechniquesSmall techniquesSmallThird = new TechniquesSmall("Toaster", 2018, 0.5);
-
-        techniquesSmall.add(techniquesSmallFirst);
-        techniquesSmall.add(techniquesSmallSecond);
-        techniquesSmall.add(techniquesSmallThird);
-
-        for (TechniquesSmall product:techniquesSmall) {
-            System.out.println(product.Info());
-            System.out.println(product.countDate());
-            System.out.println(product.needDelivery());
-        }
-
-        ArrayList <TechniquesBig> techniquesBig = new ArrayList<>();
+        List<TechniquesBig> techniquesBig = new ArrayList<>();
         TechniquesBig techniquesBigFirst = new TechniquesBig("Refrigerator", 2019, 50.5);
         TechniquesBig techniquesBigSecond = new TechniquesBig("Washer", 2015, 25.0);
         TechniquesBig techniquesBigThird = new TechniquesBig("Stove", 2010, 32.2);
@@ -30,7 +15,28 @@ public class Test  {
         techniquesBig.add(techniquesBigSecond);
         techniquesBig.add(techniquesBigThird);
 
-        System.out.println(techniquesBig.get(0).Info() +"\n" + techniquesBig.get(0).countDate()+ "\n" + techniquesBig.get(0).needDelivery());
+        List<TechniquesSmall> techniquesSmall = new ArrayList<>();
+        TechniquesSmall techniquesSmallFirst = new TechniquesSmall("Toaster", 2018, 5.2);
+        TechniquesSmall techniquesSmallSecond = new TechniquesSmall("Kettle", 2014, 1.5);
+        TechniquesSmall techniquesSmallThird = new TechniquesSmall("Samovar", 2009, 10.2);
 
+        techniquesSmall.add(techniquesSmallFirst);
+        techniquesSmall.add(techniquesSmallSecond);
+        techniquesSmall.add(techniquesSmallThird);
+
+        Scanner scanner = new Scanner(System.in);
+        int a = (int) (Math.random() * 3);
+        {
+            System.out.println("You must select a class:  Big techniques or automatically select a class of small equipment: B." + "\n" + "And we will turn it on!");
+            if (scanner.nextLine().equalsIgnoreCase("B")) {
+                System.out.println(a + " " + "Random number from the list of BIG equipment" + "\n" + techniquesBig.get(a).Info() + "\n" + techniquesBig.get(a).countDate() + "\n" + techniquesBig.get(a).needDelivery());
+
+            } else {
+                System.out.println(a + " " + "Random number from the list of SMALL equipment" + "\n" + techniquesSmall.get(a).Info() + "\n" + techniquesSmall.get(a).countDate() + "\n" + techniquesSmall.get(a).needDelivery());
+
+            }
+
+
+        }
     }
 }
