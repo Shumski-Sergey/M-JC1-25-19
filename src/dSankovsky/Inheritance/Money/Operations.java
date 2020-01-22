92aba26737e6cca83479edf121bee08a949d9e1f
JasonAntario <58884138+JasonAntario@users.noreply.github.com>
2020-01-22 09:36:49 +0300

    Update Operations.java
    Some Fixes

--- src/dSankovsky/Inheritance/Money/Operations.java
+++ src/dSankovsky/Inheritance/Money/Operations.java
@@ -9,9 +9,11 @@ public class Operations implements MoneyOperation {
     public void sumMoney(Money money1, Money money2) {
         long rubels = 0;
         char pennys = 0;
-        if ((money1.getPenny() + money2.getPenny()) > 100) {
+        if ((money1.getPenny() + money2.getPenny()) >= 100) {
             pennys = (char) ((money1.getPenny() + money2.getPenny()) % 100);
             rubels = ((money1.getPenny() + money2.getPenny()) - pennys) / 100;
+        }else{
+            pennys = (char)(money1.getPenny() + money2.getPenny());
         }
         //TODO make another penny output
         System.out.println((money1.getRuble() + money2.getRuble() + rubels) + "," + ((int) pennys));
