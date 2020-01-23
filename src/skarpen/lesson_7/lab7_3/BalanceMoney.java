0f63af783b77f49f71d89381233120c399c5a7ae
SergeyKarpen <1488karpen@gmail.com>
2020-01-23 17:54:02 +0300

    M-JC1-25-19-add 7_3 new

--- src/skarpen/lesson_7/lab7_3/BalanceMoney.java
+++ src/skarpen/lesson_7/lab7_3/BalanceMoney.java
@@ -2,28 +2,22 @@ package src.skarpen.lesson_7.lab7_3;
 
 import java.util.Scanner;
 
-public class BalanceMoney  implements InterfaceCashMachine {
+public class BalanceMoney implements InterfaceCashMachine {
 
-        public String balanceMoney(CashMachine cashMachine)
-    {
+    public String balanceMoney(CashMachine cashMachine) {
         Scanner sc = new java.util.Scanner(System.in);
         System.out.println(cashMachine.allMoney() + " All money in ATM" + "\n" + "select denomination:" + "\n" + " 20 / 50 / 100 "); //номинал купюры
         int newGreenback = sc.nextInt();
         System.out.println("amount needed for removal: "); //Сколько купюр надо снять
         int howNewGreenback = sc.nextInt();
         if (newGreenback == Constants.GREENBACKTWENTY) {
-            System.out.println(cashMachine.allMoney() - howNewGreenback * newGreenback);
-        }
-        if (newGreenback == Constants.GREENBACKFIFTY) {
-            System.out.println(cashMachine.allMoney() - howNewGreenback * newGreenback );
-        }
-        if (newGreenback == Constants.GREENBACKHUNDRED) {
-            System.out.println(cashMachine.allMoney() - howNewGreenback * newGreenback );
-        } else {
-            System.out.println("Probably you input incorrect number");
-        }
+            System.out.println(cashMachine.allMoney() - howNewGreenback * newGreenback);}
+        else if (newGreenback == Constants.GREENBACKFIFTY) {
+                System.out.println(cashMachine.allMoney() - howNewGreenback * newGreenback);}
+        else if (newGreenback == Constants.GREENBACKHUNDRED) {
+                    System.out.println(cashMachine.allMoney() - howNewGreenback * newGreenback);
+                } else System.out.println("Probably you input incorrect number");
 
-        return cashMachine.allMoney()+ " Balance ATM";
+        return cashMachine.allMoney() + " Balance ATM";
     }
-
 }
