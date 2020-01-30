381217c08acbad344ad01a1f2e7b8e42876bb7d5
Yuhnovskaya <Yuhnovskaya@gmail.com>
2020-01-30 15:39:15 +0300

    Task1 from First control added

---
+++ src/yuhnovskaya/FirstControl/Task1/DigitsNum.java
@@ -0,0 +1,22 @@
+package src.yuhnovskaya.FirstControl.Task1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DigitsNum {
+    String str;
+
+    public DigitsNum(String str) {
+        this.str = str;
+    }
+
+    public static int DigitsNum(String str){
+        Pattern pattern=Pattern.compile("[0-9]");
+        Matcher matcher=pattern.matcher(str);
+        int num=0;
+        while (matcher.find()){
+            num=num+1;
+        }
+        return num;
+    }
+}
---
+++ src/yuhnovskaya/FirstControl/Task1/Runner.java
@@ -0,0 +1,20 @@
+package src.yuhnovskaya.FirstControl.Task1;
+//В данной строке найти количество цифр. Вывести количество и сумму цифр.(С клавиатуры)
+
+import java.util.Scanner;
+
+import static src.yuhnovskaya.FirstControl.Task1.DigitsNum.DigitsNum;
+import static src.yuhnovskaya.FirstControl.Task1.DigitsSum.DigitsSum;
+
+public class Runner {
+
+    public static void main(String[] avgs){
+        Scanner scanner=new Scanner(System.in);
+        String str=scanner.nextLine();
+        int num=DigitsNum(str);
+        int sum=DigitsSum(str);
+
+        System.out.println("Number of digits: "+num);
+        System.out.println("Sum of digits: "+sum);
+    }
+}
