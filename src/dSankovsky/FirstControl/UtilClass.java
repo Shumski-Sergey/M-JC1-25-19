133951b1b3ed5629a3ec5f87c089e8015182371a
JasonAntario <jasonantario@gmail.com>
2020-01-30 19:13:55 +0300

    First Control and Task from Lab
    First Control:
    Task1 - enter the line, find only numbers and their amount, calculate sum of numbers
    Task2 - swap even element and his left neighbor
    Task3 - get 5 lines. With cycle find the longest line (or many) and print in console
    UtilClass - for easy life
    
    LabWork:
    Chars and UniqueChars - find in line amount of every letter/number/letter in lower case/letter in higher case

---
+++ src/dSankovsky/FirstControl/UtilClass.java
@@ -0,0 +1,46 @@
+package dSankovsky.FirstControl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * UtilClass for easy life
+ *
+ * @author Dmitry Sankovsky
+ */
+
+public class UtilClass {
+    //get line from console and return one String
+    public String EnterTheLine() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the line: ");
+        return reader.readLine();
+    }
+
+    //get amount of lines and add many lines in List
+    public List<String> EnterManyLines() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int amount;
+        do {
+            System.out.println("Enter amount of lines: ");
+            amount = Integer.parseInt(reader.readLine());
+        } while (amount < 0);
+        List<String> manyLines = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            manyLines.add(EnterTheLine());
+        }
+        return manyLines;
+    }
+
+    //print all lines from List
+    public void PrintAllLines(List<String> manyLines) {
+        System.out.println("Your new line, enjoy: ");
+        for (String line : manyLines) {
+            System.out.print(line + "   ");
+        }
+    }
+}
+
---
+++ src/dSankovsky/LabWork/Basket.java
@@ -0,0 +1,13 @@
+package dSankovsky.LabWork;
+
+import java.util.List;
+
+public interface Basket {
+
+    void addProduct(String product, int quantity);
+    void removeProduct(String product);
+    void updateProductQuantity(String product, int quantity);
+    void clear();
+    List<String>getProducts();
+    int getProductQuantity(String product);
+}
