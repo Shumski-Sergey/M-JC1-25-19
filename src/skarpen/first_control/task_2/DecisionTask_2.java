4a60b6292e802928b23e47a15bfc92f4cd63459f
skarpen <1488karpen@gmail.com>
2020-01-31 01:08:18 +0300

    M-JC1-25-19-create FirstControl Task2

--- src/skarpen/first_control/task_2/DecisionTask_2.java
+++ src/skarpen/first_control/task_2/DecisionTask_2.java
@@ -1,22 +1,39 @@
 package src.skarpen.first_control.task_2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DecisionTask_2 {
+
     public static void main(String[] args) {
-      /*  //2. Заменить каждый элемент списка с четным номером на соседний слева элемент.
-        ArrayList<Object> arrayList = new ArrayList<>();
+        //2. Заменить каждый элемент списка с четным номером на соседний слева элемент.
+        List<Integer> arrayList = new ArrayList<>();
 
-        Random random = new Random();
         for (int i = 0; i < 10; i++) {
-            arrayList.add((random.nextInt(5)));
+            arrayList.add((i));
         }
-        System.out.println(arrayList.toString());
-
-        for(int i = 0; i< arrayList)
-       // if(arrayList.get()% = 0)
-
-
-*/
-
+        for (Integer d : arrayList) {
+            System.out.print(d + " ");
         }
+        System.out.println("\n");
+        System.out.println(arrayList.size());
+        System.out.println("\n");
+
+
+        for (int i = 1; i < arrayList.size(); i++) {
+            if (arrayList.indexOf(i) % 2== 0) {
+                arrayList.set(i, i-1);
+            }
+        }
+        for (Object asd : arrayList) {
+            System.out.print(asd + " ");
+        }
+
+        System.out.println("\n");
+        System.out.println(arrayList.size());
+        System.out.println("\n" + "the end");
+
     }
+    }
+
 
