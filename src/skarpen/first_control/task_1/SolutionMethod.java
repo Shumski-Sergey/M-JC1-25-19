35bfe65640ebd55fddcf6cfc20ada5429d0b3115
SergeyKarpen <1488karpen@gmail.com>
2020-01-31 19:29:03 +0300

    M-JC1-25-19-create FirstControl

--- src/skarpen/first_control/task_1/SolutionMethod.java
+++ src/skarpen/first_control/task_1/SolutionMethod.java
@@ -8,26 +8,32 @@ public class SolutionMethod {
     public void AmountAndNumberOfDigits() {
         System.out.println("See the line with numbers to solve problem 1:");
 
-        // We call the 'Sсanner' method for line input
+        /*
+         *** We call the 'Sсanner' method for line input
+         */
         Scanner scanner = new Scanner(System.in); //
         String string = scanner.nextLine();
 
-        //we define the number of digits in the line
+        /*
+        *** We define the number of digits in the line
+         */
         Variables.numberOfDigits  = string.length() - string.replaceAll("\\d+", "").length();
         System.out.print("The number of digits in the entered string: " + Variables.numberOfDigits);
 
-        //Using a regular expression, we select numbers from the string
+        /*
+        ***  Using a regular expression, we select numbers from the string
+         */
         Pattern pattern = Pattern.compile("[0-9]");
         Matcher matcher = pattern.matcher(string);
 
-        //Find the numbers and use the 'parseInt' method to redefine the string into numbers
+        /*
+        *** Find the numbers and use the 'parseInt' method to redefine the string into numbers
+         */
         while (matcher.find()) {
             Variables.sumOfDigits += Integer.parseInt(matcher.group());
         }
         System.out.print("\n" + "Sum of numbers: "+ Variables.sumOfDigits);
-
         scanner.close();
-
         System.out.println("The end");
     }
 }
--- src/skarpen/first_control/task_2/DecisionTask_2.java
+++ src/skarpen/first_control/task_2/DecisionTask_2.java
@@ -1,39 +1,14 @@
 package src.skarpen.first_control.task_2;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class DecisionTask_2 {
 
     public static void main(String[] args) {
         //2. Заменить каждый элемент списка с четным номером на соседний слева элемент.
-        List<Integer> arrayList = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            arrayList.add((i));
-        }
-        for (Integer d : arrayList) {
-            System.out.print(d + " ");
-        }
-        System.out.println("\n");
-        System.out.println(arrayList.size());
-        System.out.println("\n");
-
-
-        for (int i = 1; i < arrayList.size(); i++) {
-            if (arrayList.indexOf(i) % 2== 0) {
-                arrayList.set(i, i-1);
-            }
-        }
-        for (Object asd : arrayList) {
-            System.out.print(asd + " ");
-        }
-
-        System.out.println("\n");
-        System.out.println(arrayList.size());
-        System.out.println("\n" + "the end");
-
+        ServiceArray serviceArray = new ServiceArray();
+        serviceArray.ArrayFillings();
     }
-    }
+}
+
+
 
 
