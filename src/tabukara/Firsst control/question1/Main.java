abc0dc8934c9473e3e937413e0d18c7afa05ba34
Taha-sin <taha0abokra7@gmail.com>
2020-02-02 22:22:14 +0300

    added comment not ready to the file  first control

--- src/tabukara/Firsst control/question1/Main.java
+++ src/tabukara/Firsst control/question1/Main.java
@@ -1,26 +1,10 @@
 package question1;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-//not ready
+
 public class Main {
-    public static  void main (String [] args){
-        Scanner sc = new Scanner(System.in);
-        ArrayList <Integer> dj = new ArrayList<>();
-
-        for (int i=0;i<=3;i++){
-            System.out.print("Enter the Quantity of digits: ");
-            dj.add(sc.nextInt());
-        }
-
-        int sum = 0;
-        for (int i = 0 ; i < dj.size(); i++){
-            sum += dj.get(i);
-        }
-        System.out.print("the digits are:  " + dj);
-        System.out.print("\n The quantity of numbers is: " + dj.size());
-        System.out.print(" \n The sum of the numbers is : " + sum);
-
-
+    public static void main (String[] args){
+        Q1 nq1 = new Q1();
+        nq1.digits();
     }
 }
---
+++ src/tabukara/Firsst control/question3/Q3.java
@@ -0,0 +1,35 @@
+package question3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q3 {
+    public  void maximum() {
+        final int size = 5;
+
+        //creating the list of strings
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> str = new ArrayList<String>();
+        for (int i = 0; i < size; i++) {
+            System.out.print(" Enter the String for " + i + " index: ");
+            str.add(sc.nextLine());
+        }
+        System.out.print("The list of strings is: " + str);
+
+        //finding the longest string
+        int max = str.get(0).length();
+        for (int i = 0; i < size; i++) {
+            if (max <= str.get(i).length()) {
+                max = str.get(i).length();
+
+            }
+        }
+        //displaying the longest string
+        for (int i = 0; i < size; i++) {
+            if (str.get(i).length() == max) {
+                System.out.print("\nThe longest string with the index " + i + ": " + str.get(i));
+            }
+        }
+    }
+}
+
