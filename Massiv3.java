600a08e28758c2d61a29521f22b934ef43eda3d7
sshumsky <shumski.s.a@gmail.com>
2020-02-03 22:25:39 +0300

    Clear some unused files

--- Massiv3.java
+++
@@ -1,9 +0,0 @@
-package a.mitsura;
-
-public class Massiv3 {
-    public static void main(String [] args){
-        for(int i=90; i>=0; i-=5){
-            System.out.println(i);
-        }
-    }
-}
