e6375940b01888516630b2f69da8c217f08449d6
mxfrei <dmitry19@inbox.ru>
2020-02-06 17:57:36 +0300

    Some changes with names of classes (first char is Upper).

--- src/dbesan/lab8collections/a1/Runner.java
+++ src/dbesan/lab8collections/a1/Runner.java
@@ -1,11 +1,13 @@
 package dbesan.lab8collections.a1;
 
+import static dbesan.lab8collections.a1.Remover.remove;
+
 public class Runner {
     public static final int NORMAL_RATE = 5;
 
     public static void main(String[] args) {
 
-        remover.remove();
+        remove();
     }
 
 
