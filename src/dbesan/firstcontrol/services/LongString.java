88fef423b626e45ba403005b2d302050dd99343a
mxfrei <dmitry19@inbox.ru>
2020-02-06 19:04:52 +0300

    Some bicycle was rewrites.

--- src/dbesan/firstcontrol/services/LongString.java
+++ src/dbesan/firstcontrol/services/LongString.java
@@ -3,21 +3,59 @@ package dbesan.firstcontrol.services;
 import dbesan.firstcontrol.services.impl.UniqueService;
 
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
 
 import static dbesan.firstcontrol.util.Constants.COUNTTHIRDMISSION;
 import static dbesan.firstcontrol.util.InputText.Text;
 
 public class LongString implements UniqueService {
-    private static String text;
-    private static Map<String, Integer> map = new TreeMap<>(); //uses for saving strings and lenght
-    private static int maxCount = 0; //uses for count of longest strings
+
+
+    public static ArrayList<String> getMaxlist() {
+        return maxlist;
+    }
+
+    private static ArrayList<String> maxlist = new ArrayList<>();
 
     public static void longest() {
         ArrayList<String> list = new ArrayList<>();
-        Scanner TextScanner = new Scanner(System.in);
+
+        int maxlenght = 0;
+        for (int i = 0; i < COUNTTHIRDMISSION; i++) {
+            //cycle saving strings in array
+            list.add(Text());
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).length() > maxlenght) {
+                maxlenght = list.get(i).length();
+            }
+            if (list.get(i).length() == maxlenght) {
+                maxlist.add(list.get(i));
+            }
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        System.out.println("Результат:");
+        String result = "";
+        for (String i : maxlist) {
+            result += "Самая длинная строка: " + i + " " +
+                    "Длиной: " + i.length() + "\n";
+        }
+        return result;
+    }
+}
+       /*
+
+        My bicycle. LOL
+
+         private static int maxCount = 0; //uses for count of longest strings
+          private static String text;
+    private static Map<String, Integer> map = new TreeMap<>(); //uses for saving strings and lenght
+
+
+        ArrayList<String> list = new ArrayList<>();
         int countStrings = 0; //how much strings
         for (int i = 0; i < COUNTTHIRDMISSION; i++) {
             //cycle saving strings in array and its count
@@ -57,6 +95,5 @@ public class LongString implements UniqueService {
             result += "Самая длинная строка: " + entry.getKey() + " " +
                     "Длиной: " + entry.getValue() + "\n";
         }
-        return result;
-    }
-}
+        return result;*/
+   
