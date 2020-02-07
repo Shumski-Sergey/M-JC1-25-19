05b8a1edf3514bea4f5e76ce2a7918d9ca125b30
Yuhnovskaya <Yuhnovskaya@gmail.com>
2020-02-08 00:15:41 +0300

    Tasks 2 and 3 from Streams API added

--- src/yuhnovskaya/streamAPI/Task2.java
+++ src/yuhnovskaya/streamAPI/Task2.java
@@ -12,14 +12,13 @@ import java.util.Optional;
 public class Task2 {
     public  static void main(String[] avgs) throws IOException {
         ArrayList<String>list=new ArrayList<>();
-        ArrayList<String> finallist=new ArrayList<>();
         BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
         while (true){
             String str=reader.readLine();
             if (str.isEmpty())break;
             list.add(str);
         }
-        Optional<String> result = list.stream().filter(x->x.toString().length()==6).reduce((x1, x2)->x1+","+x2);
+        Optional<String> result = list.stream().filter(x->x.toString().length()>6).reduce((x1, x2)->x1+","+x2);
         System.out.print(result);
     }
 }
