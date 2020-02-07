b7baa0c6885cbe78d6c7b0c396d64597f68e848b
Artyom Antonovich <artemka6996@gmail.com>
2020-02-07 17:36:45 +0300

    little changes

--- src/aantonovich/Stream/Task2.java
+++ src/aantonovich/Stream/Task2.java
@@ -1,9 +1,8 @@
 package aantonovich.Stream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // Считать с консоли данные(массив слов). Записать в другую переменную String строку
 // (через запятую слова), длина которых строго больше 6. В конце запятой не должно быть
@@ -13,9 +12,7 @@ public class Task2 {
         System.out.println("Enter the string:");
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
-        String[] s1 = s.split(" ");
-        ArrayList<String> list = new ArrayList<>(Arrays.asList(s1));
-        String s2 = list.stream()
+             String s2 = Stream.of(s.split(" "))
                 .filter(x -> x.length() > 6)
                 .collect(Collectors.joining(", "));
         System.out.println(s2);
