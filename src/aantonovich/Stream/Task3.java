28eb05e67cdaa9dcea4aa6432b9198cab19c6da6
Artyom Antonovich <artemka6996@gmail.com>
2020-02-08 13:56:29 +0300

    add task3 in package Stream

---
+++ src/aantonovich/Stream/Task3.java
@@ -0,0 +1,21 @@
+package aantonovich.Stream;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// Считать с консоли данные. Вывести во вторую переменную все числа,
+// которые есть в первом файле. Числа выводить через пробел.
+// Пример - 12 text var2 14 8v 1 Результат: 12 14 1
+public class Task3 {
+    public static void main (String[] args) {
+
+        System.out.println("Enter the string:");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String s2 = Stream.of(s.split(" "))
+                .filter(x -> x.matches("^\\d+$"))
+                .collect(Collectors.joining(" "));
+        System.out.println(s2);
+    }
+}
