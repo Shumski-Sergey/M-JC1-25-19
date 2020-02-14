7c33faf02f62ae1735746d1223ff96e10669a5b3
VetashkovPavel <Pavanaindra@mail.ru>
2020-02-14 19:22:23 +0200

    .

--- src/pvetashkov/Generics/labGenericsA1.java
+++ src/pvetashkov/Generics/labGenericsA1.java
@@ -1,19 +1,19 @@
-package src.pvetashkov.generics.lab.generics;
+package src.pvetashkov.Generics;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class A1 {// as Velcom "And Tomorrow will be youth"
+public class labGenericsA1 {
     public static void main(String []args) throws IOException{
-        System.out.println("Enter any nums with commas and brakes ");// comma это запятая. свят-свят...
+        System.out.println("Enter any nums with commas and brakes "); // comma это запятая. свят-свят...
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         String str=new String();
         str=br.readLine();
         System.out.println("You entered this nums "+ str);
         String[] arr=str.split(", +");// раздел по запятой и пробелу/ам
         Set<String> modernized=new HashSet<>(Arrays.asList(arr));
-        System.out.println("without repeatted nums "+modernized);
+        System.out.println("without repeated nums "+modernized);
     }
 }
