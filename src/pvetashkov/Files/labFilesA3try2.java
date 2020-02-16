97228179b6b09dcbe00f86f10fb7d058e25955a1
VetashkovPavel <Pavanaindra@mail.ru>
2020-02-16 20:58:26 +0200

    .

--- src/pvetashkov/Files/labFilesA3try2.java
+++ src/pvetashkov/Files/labFilesA3try2.java
@@ -8,24 +8,23 @@ public class labFilesA3try2 {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         FileWriter fwr=new FileWriter("C:\\Users\\Pavel\\Desktop\\labafilesA3.txt");
         String str=br.readLine();
+        if (str.split(" +").equals("#esc")){
+            System.out.println("ESCAPE was found");
+                return;}
         fwr.write(str);
         br.close();
         fwr.close();
     }
     public static void main(String []args) throws IOException {
         File labafilesA3=new File("C:\\Users\\Pavel\\Desktop\\labafilesA3.txt");
-        if (!labafilesA3.exists()) {
-            labafilesA3.createNewFile();
+        if (labafilesA3.exists()) {
+            labafilesA3.delete();// если уже имеется - сносим его.
+            System.out.println("Old file was deleted. New file was created");
         }
         else {
-            System.out.println("It is. del old.");
+            labafilesA3.createNewFile();
         }
         System.out.println("Type here smt lowcase. To exit type #esc ");
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String str=br.readLine();
-        do Reading();
-        while (str.split(" +").equals("#esc"));
-        {
-        return;}
+        Reading();
     }
 }
