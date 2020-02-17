3a99ae46aa73db70f7963f3368b9aaaf3968bf59
Temofeich <artem_antonovich_1988@mail.ru>
2020-02-17 21:29:40 +0300

    added package InputAndOutputStreams and other little changes

---
+++ src/aantonovich/InputAndOutputStreams/Task3.java
@@ -0,0 +1,28 @@
+package aantonovich.InputAndOutputStreams;
+//Организовать построчное считывание данных, введенных с консоли, и их запись в файл.
+// Признаком окончания записи (конец работы программы) является строка со словом «#ESC»
+import java.io.*;
+import java.util.Objects;
+
+public class Task3 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите строки текста. Для завершения введите ESC");
+        File file = new File("src\\aantonovich\\InputAndOutputStreams\\task3.txt");
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+        String string = null;
+        while (true) {
+            try {
+                string = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (Objects.equals(string, "ESC")) break;
+            writer.write(Objects.requireNonNull(string) + "\n");
+        }
+              writer.close();
+              bufferedReader.close();
+        }
+
+    }
--- src/aantonovich/StreamAPI/Test.java
+++ src/aantonovich/StreamAPI/Test.java
@@ -1,4 +1,4 @@
-package aantonovich.Stream;
+package aantonovich.StreamAPI;
 
 public class Test {
     public static void main(String[] args) {
