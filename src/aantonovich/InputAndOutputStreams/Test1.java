1617d93188ab8946f7812db803b4fb315bf5b691
Temofeich <artem_antonovich_1988@mail.ru>
2020-02-18 23:06:41 +0300

    added task for InputAndOutputStreams

---
+++ src/aantonovich/InputAndOutputStreams/Test1.java
@@ -0,0 +1,37 @@
+package aantonovich.InputAndOutputStreams;
+
+// найти совпадения начальной буквы слова и последней буквы предыдущего слова
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Test1 {
+    private static final File FILE_DIRECTORY = new File("src" +File.separator+ "aantonovich"+ File.separator+ "InputAndOutputStreams");
+    public static void main (String[] args){
+        File file = new File(FILE_DIRECTORY, "test.txt");
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(file)))){
+            String previousWord = null;
+            if (scanner.hasNext()){
+                previousWord = scanner.next();
+        }
+            while (scanner.hasNext()){
+                String nextWord = scanner.next();
+                if (lastLetter(previousWord).equalsIgnoreCase(firstLetter(nextWord))){
+                    System.out.println(previousWord + " : " + nextWord);
+                }
+                previousWord = nextWord;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+}
+private  static  String lastLetter(String word) {
+        return word.substring(word.length() - 1);
+}
+private static String firstLetter(String word){
+        return word.substring(0, 1);
+}
+}
